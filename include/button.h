0c64d33f7dedea6e2f428dac8f5165210abe6849
Kushal Subedi <kushalsubedi2@gmail.com>
2021-06-28 18:38:19 +0545

    Removed unnecessary files

--- include/button.h
+++
@@ -1,7 +0,0 @@
-#include <wx/wx.h>
-
-class Button : public wxFrame{
-    public:
-        Button(const wxString &title);
-        void OnQuit(wxCommandEvent &event);
-};
--- include/icon.h
+++
@@ -1,5 +0,0 @@
-#include <wx/wx.h>
-class Icon: public wxFrame{
-    public:
-        Icon(const wxString &title);
-};
